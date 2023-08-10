b4926b1289b8381893d78db2c854d403c2babd76
Joaquin <joaquinpalacio2311@gmail.com>
2023-08-10 17:26:19 -0300

    agregado de boton con js

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", () => {
+  const scrollButton = document.getElementById("scroll-to-top");
+  if (window.scrollY > 300) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+});
+
+document.getElementById("scroll-to-top").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
